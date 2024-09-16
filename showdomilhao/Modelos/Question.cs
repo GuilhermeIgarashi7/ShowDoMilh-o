dacc2a78798629e29df1b9a960471825c5e9de29
Guilherme <gui.igarashi@hotmail.com>
2024-09-16 09:54:09 -0300

    commit pre intervalo

---
+++ showdomilhao/Modelos/Question.cs
@@ -0,0 +1,23 @@
+namespace Modelos
+{
+
+    public class Question
+    {
+        public string QuestionM;
+
+        public string FirstResponse;
+
+        public string SecondResponse;
+
+        public string ThirdResponse;
+
+        public string FourthResponse;
+
+        public string FifthResponse;
+
+        public int QuestionLevel;
+
+
+
+    }
+}
