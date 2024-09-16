84647c8ac0862d6b78e640daf7d478925b125fcf
Guilherme <gui.igarashi@hotmail.com>
2024-09-16 11:48:44 -0300

    tá horroroso

--- showdomilhao/Modelos/Question.cs
+++ showdomilhao/Modelos/Question.cs
@@ -1,5 +1,7 @@
 namespace Modelos
+
 {
+
 
     public class Question
     {
@@ -17,7 +19,74 @@ namespace Modelos
 
         public int QuestionLevel;
 
+        public int CorrectResponse = 0;
 
+
+
+        //----------------------------------------------------------------------------------------------
+
+        private Label labelPergunta;
+
+        private Button button01;
+
+        private Button button02;
+
+        private Button button03;
+
+        private Button button04;
+
+        private Button button05;
+
+        public Question(Label LabelQuestion, Button FirstButtonC, Button SecondButtonC, Button ThirdButtonC, Button FourthButtonC, Button FifthButtonC)
+        {
+            labelPergunta = LabelQuestion;
+            button01 = FirstButtonC;
+            button02 = SecondButtonC;
+            button03 = ThirdButtonC;
+            button04 = FourthButtonC;
+            button05 = FifthButtonC;
+        }
+
+        public void Drawn()
+        {
+            labelPergunta.Text = QuestionM;
+            button01.Text = FirstResponse;
+            button02.Text = SecondResponse;
+            button03.Text = ThirdResponse;
+            button04.Text = FourthResponse;
+            button05.Text = FifthResponse;
+        }
+
+        private Button QualBnt(int CR)
+        {
+            if (CR == 1)
+            {
+                return button01;
+            }
+            else if (CR == 2)
+            {
+                return button02;
+            }
+        }
+
+        public bool VerifyResponse(int CR)
+        {
+            if  (CR == CorrectResponse)
+            {
+                var Btn = QualBnt(CR);
+                    Btn = BackgroundColor = Color.Green;
+            }
+            else
+            {
+                return  false;
+            }
+        }
 
     }
+
+
+
+
+
+
 }
